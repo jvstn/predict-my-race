7dc7a8e5326f2c3d773033cbdc603181e37d14d1
jmcfadd <justinmcfadd4@gmail.com>
2021-04-22 10:55:13 -0400

    Added icons to menu Items

--- src/components/Dashboard/Navbar/NavbarElements/MenuItem.tsx
+++ src/components/Dashboard/Navbar/NavbarElements/MenuItem.tsx
@@ -1,14 +1,19 @@
 import styled from 'styled-components';
-import React, { ReactElement, ReactNode } from 'react'
+import { ReactElement, ReactNode } from 'react'
 import { Link } from 'react-router-dom'
+import { IconType } from 'react-icons/lib';
+import { palette } from '../../../../theme';
+
 interface Props {
   to: string;
   children: ReactNode;
+  Icon: IconType;
 }
 
-function MenuItem({to, children}: Props): ReactElement {
+function MenuItem({to, children, Icon }: Props): ReactElement {
   return (
     <LinkWrap>
+    <Icon color={'white'} />
     <StyledLink to={to}>{children}</StyledLink>
     </LinkWrap>
   )
@@ -17,11 +22,18 @@ function MenuItem({to, children}: Props): ReactElement {
 const LinkWrap = styled.div`
   display: flex;
   height: 5vh;
+  align-items: center;
+  margin-left: 3vh;
+  margin-right: auto;
 `
 
 const StyledLink = styled(Link)`
   color: white;
+  font-weight: bold;
+  text-decoration: none;
+  margin-left: 1vh;
 `
+
 
 
 export default MenuItem
--- src/components/Dashboard/Navbar/index.tsx
+++ src/components/Dashboard/Navbar/index.tsx
@@ -1,13 +1,21 @@
 import React, { ReactElement } from "react";
 import { Logo, MenuItem, NavbarContainer } from "./NavbarElements";
+import { MdDashboard, MdInfo } from "react-icons/md";
+import {FaStrava} from "react-icons/fa";
 
 function Navbar(): ReactElement {
   return (
     <NavbarContainer>
       <Logo />
-      <MenuItem to="/dashboard">Prediction</MenuItem>
-      <p>Algorithms</p>
-      <p>Connect to Strava</p>
+      <MenuItem to="/dashboard" Icon={MdDashboard}>
+        Prediction
+      </MenuItem>
+      <MenuItem to="/information" Icon={MdInfo}>
+        Information
+      </MenuItem>
+      <MenuItem to="/connect" Icon={FaStrava}>
+        Connect to Strava
+      </MenuItem>
     </NavbarContainer>
   );
 }
